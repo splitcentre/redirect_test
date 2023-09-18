cf9f13952701f901697061e3d54693231b22f9a0
splitcentre <raihanalfianpratama@gmail.com>
2023-09-18 18:49:31 +0700

    change homepage and register

--- app/src/main/java/com/example/redirect/Homepage.kt
+++ app/src/main/java/com/example/redirect/Homepage.kt
@@ -2,11 +2,20 @@ package com.example.redirect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.redirect.databinding.ActivityHomepageBinding
 
 class Homepage : AppCompatActivity() {
+    private lateinit var binding: ActivityHomepageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        binding=ActivityHomepageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.getStringExtra(MainActivity.EXTRA_NAME)
+        val phoneNumber = intent.getStringExtra(MainActivity.EXTRA_PHONE_NUMBER) // Retrieve the phone number
+
+        binding.nama.text = "Name: $name"
+        binding.phone.text = "Phone Number: $phoneNumber"
 
     }
 }
--- app/src/main/java/com/example/redirect/MainActivity.kt
+++ app/src/main/java/com/example/redirect/MainActivity.kt
@@ -2,20 +2,31 @@ package com.example.redirect
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
+import com.example.redirect.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_PHONE_NUMBER = "extra_phone_number"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val redirectbutton = findViewById<MaterialButton>(R.id.redirectto)
-        redirectbutton.setOnClickListener{
-            val intel=Intent(this, Homepage::class.java)
-            startActivity(intel)
+        binding.redirectto.setOnClickListener {
+            val name = binding.username.text.toString()
+            val phoneNumber = binding.nomerHp.text.toString() // Get the phone number
+
+            val intent = Intent(this@MainActivity, Homepage::class.java)
+            intent.putExtra(EXTRA_NAME, name)
+            intent.putExtra(EXTRA_PHONE_NUMBER, phoneNumber)
+            startActivity(intent)
         }
     }
 }
